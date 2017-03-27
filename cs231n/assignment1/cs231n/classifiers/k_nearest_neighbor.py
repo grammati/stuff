f495651f03c8cb1cb4f25114782a1fdab07b5295
Chris Perkins <chrisperkins99@gmail.com>
2017-03-27 16:46:57 -0600

    Assignment 1, part 1 - KNN

--- cs231n/assignment1/cs231n/classifiers/k_nearest_neighbor.py
+++ cs231n/assignment1/cs231n/classifiers/k_nearest_neighbor.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import defaultdict
 
 class KNearestNeighbor(object):
   """ a kNN classifier with L2 distance """
@@ -8,7 +9,7 @@ class KNearestNeighbor(object):
 
   def train(self, X, y):
     """
-    Train the classifier. For k-nearest neighbors this is just 
+    Train the classifier. For k-nearest neighbors this is just
     memorizing the training data.
 
     Inputs:
@@ -19,7 +20,7 @@ class KNearestNeighbor(object):
     """
     self.X_train = X
     self.y_train = y
-    
+
   def predict(self, X, k=1, num_loops=0):
     """
     Predict labels for test data using this classifier.
@@ -33,7 +34,7 @@ class KNearestNeighbor(object):
 
     Returns:
     - y: A numpy array of shape (num_test,) containing predicted labels for the
-      test data, where y[i] is the predicted label for the test point X[i].  
+      test data, where y[i] is the predicted label for the test point X[i].
     """
     if num_loops == 0:
       dists = self.compute_distances_no_loops(X)
@@ -49,7 +50,7 @@ class KNearestNeighbor(object):
   def compute_distances_two_loops(self, X):
     """
     Compute the distance between each test point in X and each training point
-    in self.X_train using a nested loop over both the training data and the 
+    in self.X_train using a nested loop over both the training data and the
     test data.
 
     Inputs:
@@ -71,7 +72,7 @@ class KNearestNeighbor(object):
         # training point, and store the result in dists[i, j]. You should   #
         # not use a loop over dimension.                                    #
         #####################################################################
-        pass
+        dists[i,j] = np.sqrt(np.sum(np.square(X[i] - self.X_train[j])))
         #####################################################################
         #                       END OF YOUR CODE                            #
         #####################################################################
@@ -93,7 +94,7 @@ class KNearestNeighbor(object):
       # Compute the l2 distance between the ith test point and all training #
       # points, and store the result in dists[i, :].                        #
       #######################################################################
-      pass
+      dists[i,:] = np.sqrt(np.sum(np.square(self.X_train - X[i]), axis=1))
       #######################################################################
       #                         END OF YOUR CODE                            #
       #######################################################################
@@ -108,7 +109,7 @@ class KNearestNeighbor(object):
     """
     num_test = X.shape[0]
     num_train = self.X_train.shape[0]
-    dists = np.zeros((num_test, num_train)) 
+    dists = np.zeros((num_test, num_train))
     #########################################################################
     # TODO:                                                                 #
     # Compute the l2 distance between all test points and all training      #
@@ -121,7 +122,28 @@ class KNearestNeighbor(object):
     # HINT: Try to formulate the l2 distance using matrix multiplication    #
     #       and two broadcast sums.                                         #
     #########################################################################
-    pass
+
+    # Attempt 1 (blows up, too much memory)
+    # X_train: (N,D), X: (T,D)
+    # N = num_train; T = num_test; D = X.shape[1]
+    # assert self.X_train.shape == (N,D)
+    # assert X.shape == (T,D)
+    # dists = np.sum(np.square(self.X_train.reshape((N,1,D)) - X.reshape((1,T,D)) ), axis=2)
+
+    # X.shape == (500,3072), X_train.shape == (5000,3072)
+    # What we want is all distances: (500,5000,3072)
+    # sqrt( sum( square(a - b) ) )
+    # == sqrt( sum( a^2 + b^2 - 2ab ) )
+    # == sqrt( sum(a^2) + sum(b^2) - 2*sum(ab) )
+    Xsq = np.square(X)             # (500, 3072)
+    Tsq = np.square(self.X_train)  # (5000,3072)
+    XT = np.dot(X, self.X_train.T) # (500,5000)
+    dists = np.sqrt(
+        np.sum(Xsq, axis=1)
+          .reshape((X.shape[0],1)) # (500,1)
+        + np.sum(Tsq, axis=1)      # (5000,)
+        - (2 * XT))                # (500,5000)
+
     #########################################################################
     #                         END OF YOUR CODE                              #
     #########################################################################
@@ -138,7 +160,7 @@ class KNearestNeighbor(object):
 
     Returns:
     - y: A numpy array of shape (num_test,) containing predicted labels for the
-      test data, where y[i] is the predicted label for the test point X[i].  
+      test data, where y[i] is the predicted label for the test point X[i].
     """
     num_test = dists.shape[0]
     y_pred = np.zeros(num_test)
@@ -153,7 +175,7 @@ class KNearestNeighbor(object):
       # neighbors. Store these labels in closest_y.                           #
       # Hint: Look up the function numpy.argsort.                             #
       #########################################################################
-      pass
+      closest_y = self.y_train[ np.argsort(dists[i])[:k] ]
       #########################################################################
       # TODO:                                                                 #
       # Now that you have found the labels of the k nearest neighbors, you    #
@@ -161,10 +183,13 @@ class KNearestNeighbor(object):
       # Store this label in y_pred[i]. Break ties by choosing the smaller     #
       # label.                                                                #
       #########################################################################
-      pass
+      counts = defaultdict(int)
+      for y in closest_y:
+        counts[y] += 1
+      _,y = sorted((-v,k) for k,v in counts.iteritems())[0]
+      y_pred[i] = y
       #########################################################################
-      #                           END OF YOUR CODE                            # 
+      #                           END OF YOUR CODE                            #
       #########################################################################
 
     return y_pred
-
