ea49a816474c7c691ba855f2d854187ddaa400d7
Chris Perkins <chrisperkins99@gmail.com>
2017-03-28 13:21:26 -0600

    cs231n assignment 1, part 2 (svm), in progress

--- cs231n/assignment1/cs231n/classifiers/linear_svm.py
+++ cs231n/assignment1/cs231n/classifiers/linear_svm.py
@@ -19,28 +19,78 @@ def svm_loss_naive(W, X, y, reg):
   - loss as single float
   - gradient with respect to weights W; an array of same shape as W
   """
-  dW = np.zeros(W.shape) # initialize the gradient as zero
+  D = W.shape[0]
+  C = W.shape[1]
+  N = X.shape[0]
+  delta = 1.0
+
+  # initialize the gradient as zero
+  dW = np.zeros((D,C))
 
   # compute the loss and the gradient
   num_classes = W.shape[1]
   num_train = X.shape[0]
   loss = 0.0
-  for i in xrange(num_train):
-    scores = X[i].dot(W)
-    correct_class_score = scores[y[i]]
+
+  for im, correct_class in zip(X, y):
+    # scores: array of C stores, each being a float where a higher number means
+    # we predict a higher probability that the image of of that class
+    scores = im.dot(W) # (D,) . (D,C) == (C,)
+
     for j in xrange(num_classes):
-      if j == y[i]:
+      # SVM loss is the sum of incorrect class scores that exceed the correct
+      # class' score by more than some margin delta. Because of that, we can
+      # skip ahead when j is the correct class of the current image.
+      if j == correct_class:
         continue
-      margin = scores[j] - correct_class_score + 1 # note delta = 1
+
+      margin = scores[j] - scores[correct_class] + delta
       if margin > 0:
         loss += margin
+
+        # The following line is very hard to explain. It has to do with the
+        # gradient of the hinge-loss, which takes some fancy math to derive.
+        # Essentially it is saying this: Because the score in class j (which is
+        # not the correct class for this image) exceeds the score for the
+        # correct class by more than delta, the parameters in the j-th column
+        # of W contributed too much to scores[j] for this image. We need the
+        # parameters in column j to be smaller. How much smaller? Let's
+        # illustrate with a specific example. Let's look at when j is 7. Then
+        # the parameter at, say, W[3,7] (the parameter that decides how much
+        # the value of pixel number 3 contributes to making the image look like
+        # it's in class 7) contributed W[3,7] * im[3] to scores[7]. Similarly
+        # for other indices (other than 3), up to D (the dimension of the
+        # inputs). So the gradient on column j of W due to this input is just
+        # `im`. To see this, consider a single cell in W - the example above of
+        # W[3,7]. The contribution to the loss is W[3,7] * im[3]. Considering
+        # W[3,7] as the variable, and im[3] as a constant, the derivative of
+        # that product with respect to W[3,7] is just im[3]. So we need to add
+        # im[3] to the value of dW[3,7]. Similarly for indices other than 3. So
+        # we just add im to column j of dW.
+        dW[:, j] += im
+
+        # Now consider that calculating the score for this (incorrect) class,
+        # j, involves subtracting the score of the correct class, which is the
+        # dot product of column `c` of W with `im` (this is just due to how the
+        # hinge-loss is defined). So for every incorrect class j, the values of
+        # the parameters in the correct-class column of W have a negative
+        # contribution to scores[j].
+        dW[:, correct_class] -= im
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
 
+  # Ditto for the gradient
+  dW /= num_train
+
   # Add regularization to the loss.
   loss += 0.5 * reg * np.sum(W * W)
+
+  # Contribution to the gradient due to the regularization. I note that the 2
+  # from taking the derivative of W**2 cancels out with the 0.5 above (which is
+  # exactly the reason for including that 0.5 in the first place.)
+  dW += reg * W
 
   #############################################################################
   # TODO:                                                                     #
